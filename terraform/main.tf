cae10d8d6b10600ef867c50f3645753b70227f95
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-25 18:04:48 -0500

    iam json

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
 
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
-  assume_role_policy = temp-jose({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
       Action = "sts:AssumeRole",
