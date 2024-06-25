c6a485fc09f9d21246c16f7ca8317deb669161ee
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-25 17:18:56 -0500

    add path file test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
 import unittest
-import json
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+
 from app import app, votes
+import json
+
 
 class TestVotingApp(unittest.TestCase):
 
