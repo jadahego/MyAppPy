e08b501d43304b3ea87437b188c89693d830810f
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-25 22:23:46 -0500

    try to solve deps

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,12 +1,19 @@
 from app import app
 import awsgi
+import json
 #import sys
 
 def lambda_handler(event, context):      
     #search_path = sys.path
     #print(search_path)
+    # Convertir los objetos event y context a JSON
+    event_json = json.dumps(event, indent=4)
+    context_json = json.dumps(context, indent=4, default=str)  # default=str para manejar objetos que no son serializables por defecto
+
+    # Imprimir los objetos JSON
     print("event")
-    print(event)
+    print(event_json)
     print("context")
-    print(context)
+    print(context_json)
+    
     return awsgi.response(app, event, context)
