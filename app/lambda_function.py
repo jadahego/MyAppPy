b42467e73350c6d579c5f32b5d7688d1be684473
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-25 20:14:48 -0500

    try to solve deps

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,5 +1,8 @@
 from app.app import app
 import awsgi
+import sys
 
-def lambda_handler(event, context):
+def lambda_handler(event, context):      
+    search_path = sys.path
+    print(search_path)
     return awsgi.response(app, event, context)
