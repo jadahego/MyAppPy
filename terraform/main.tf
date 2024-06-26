ff3ee184e5452f5c7551117f9641c9edcbaebf56
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-26 11:30:45 -0500

    lambda.py act

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "bbog-my-lambda-functions-bucket-jdhg"
+  bucket = "bbog-my-lambda-functions-bucket_jdhg"
 }
 
 resource "aws_iam_role" "lambda_exec_jdhg" {
