16a931008b857fefd2ff5db1552a532a27426658
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-26 11:05:26 -0500

    lambda.py act

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,7 +112,5 @@ output "api_url" {
   value = aws_api_gateway_deployment.voting_api_deployment.invoke_url
 }
 
-output "lambda_function_arn" {
-  value = aws_lambda_function.voting_function_jdhg.arn
-}
+
 
