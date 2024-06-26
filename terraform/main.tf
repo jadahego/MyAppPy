d58c5653d00e1d6ec0e06e1a4e00be277a891d13
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-26 10:27:23 -0500

    lambda.py act

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "bbog-my-lambda-functions-bucket-jdhg"
 }
 
-resource "aws_iam_role" "lambda_exec-jdhg" {
-  name = "lambda_exec_role-jdhg"
+resource "aws_iam_role" "lambda_exec_jdhg" {
+  name = "lambda_exec_role_jdhg"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -21,15 +21,15 @@ resource "aws_iam_role" "lambda_exec-jdhg" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_exec-jdhg_policy" {
-  role       = aws_iam_role.lambda_exec-jdhg.name
+resource "aws_iam_role_policy_attachment" "lambda_exec_jdhg_policy" {
+  role       = aws_iam_role.lambda_exec_jdhg.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_lambda_function" "voting_function-jdhg" {
+resource "aws_lambda_function" "voting_function_jdhg" {
   filename         = "../app/lambda_function_payload.zip" 
   function_name    = "votingFunctionjdhg"
-  role             = aws_iam_role.lambda_exec-jdhg.arn
+  role             = aws_iam_role.lambda_exec_jdhg.arn
   handler          = "lambda_function.lambda_handler"
   source_code_hash = filebase64sha256("../app/lambda_function_payload.zip")
   runtime          = "python3.8"
@@ -94,7 +94,7 @@ resource "aws_lambda_permission" "apigw_post" {
 resource "aws_lambda_permission" "apigw_get" {
   statement_id  = "AllowAPIGatewayInvokeGET"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.voting_function-jdhg.function_name
+  function_name = aws_lambda_function.voting_function_jdhg.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.voting_api.execution_arn}/*/GET/vote"
 }
@@ -109,5 +109,5 @@ resource "aws_api_gateway_deployment" "voting_api_deployment" {
 }
 
 output "api_url" {
-  value = aws_api_gateway_deployment.voting_api_deployment.invoke_url
+  value = aws_api_gateway_rest_api.voting_api.execution_invoke_url
 }
