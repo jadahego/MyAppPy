ce9a340ed34075b79526b4f06ab6239dcc3a77e5
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-25 20:27:56 -0500

    try to solve deps

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
-from dependencies import flask
-#from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 votes = {"option1": 0, "option2": 0}
 
@@ -11,16 +10,16 @@ def home():
 
 @app.route('/vote', methods=['POST'])
 def vote():
-    data = flask.request.get_json()
+    data = request.get_json()
     option = data.get('option')
     if option in votes:
         votes[option] += 1
-        return flask.jsonify({"message": "Vote counted!"}), 200
-    return flask.jsonify({"message": "Invalid option"}), 400
+        return jsonify({"message": "Vote counted!"}), 200
+    return jsonify({"message": "Invalid option"}), 400
 
 @app.route('/results', methods=['GET'])
 def results():
-    return flask.jsonify(votes), 200
+    return jsonify(votes), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
