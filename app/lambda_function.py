a127f086501259ee4f06c8887ad47590bc3052f5
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-25 23:11:28 -0500

    lambda.py act

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,8 +1,25 @@
-import json
+from app import app
 import awsgi
+import json
 
 def lambda_handler(event, context):
-    print("Received event:", json.dumps(event))
+    # Convertir los objetos event y context a JSON
+    event_json = json.dumps(event)
+    context_json = json.dumps({
+        "function_name": context.function_name,
+        "function_version": context.function_version,
+        "invoked_function_arn": context.invoked_function_arn,
+        "memory_limit_in_mb": context.memory_limit_in_mb,
+        "aws_request_id": context.aws_request_id,
+        "log_group_name": context.log_group_name,
+        "log_stream_name": context.log_stream_name,
+    }, default=str)  # default=str para manejar objetos que no son serializables por defecto
+
+    # Imprimir los objetos JSON
+    print("event")
+    print(event_json)
+    print("context")
+    print(context_json)
 
     # Verificar la presencia de 'httpMethod' en el evento
     if 'httpMethod' not in event:
@@ -16,9 +33,11 @@ def lambda_handler(event, context):
             })
         }
 
+    # Manejar el evento con awsgi
     try:
         return awsgi.response(app, event, context)
     except Exception as e:
+        # Manejar cualquier excepción no anticipada y devolver un error 500
         error_message = f"Internal server error: {str(e)}"
         print(error_message)  # Para registro en los logs de CloudWatch
         return {
@@ -28,5 +47,4 @@ def lambda_handler(event, context):
                 'event': event
             })
         }
-
 
