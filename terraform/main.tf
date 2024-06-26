c68444629f6b68c5fe2e5008855da7eb455163e6
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-25 21:19:45 -0500

    try to solve deps

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,11 +27,11 @@ resource "aws_iam_role_policy_attachment" "lambda_exec-jdhg_policy" {
 }
 
 resource "aws_lambda_function" "voting_function-jdhg" {
-  filename         = "../lambda_function_payload.zip" 
+  filename         = "../app/lambda_function_payload.zip" 
   function_name    = "votingFunctionjdhg"
   role             = aws_iam_role.lambda_exec-jdhg.arn
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = filebase64sha256("../lambda_function_payload.zip")
+  source_code_hash = filebase64sha256("../app/lambda_function_payload.zip")
   runtime          = "python3.8"
   environment {
     variables = {
